516fe2dfd1d8fffd56a3ea3a25fd1fb1051f84a0
Nail <mazhitovnail@gmail.com>
2024-05-27 16:58:17 +0300

    commit first

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,14 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean_2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+
+        Cat cat = applicationContext.getBean("catBean", Cat.class);
+        Cat cat_2 = applicationContext.getBean("catBean", Cat.class);
+        System.out.println("Результат сравнения bean.HelloWorld и bean_2.HelloWorld: " + (bean  ==  bean_2));
+        System.out.println("Результат сравнения cat и cat_2: " + (cat == cat_2));
+
     }
 }
